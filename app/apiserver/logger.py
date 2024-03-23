21eb3bc09a4aa94e123c5938d38c710214aad614
Hang <64747715+xx529@users.noreply.github.com>
2024-03-23 22:24:04 +0800

    抓取系统其他 log 日志

--- app/apiserver/logger.py
+++ app/apiserver/logger.py
@@ -5,21 +5,41 @@ from loguru import logger
 from app.config import app_conf, log_conf
 from app.schema.enum import LoggerTypeEnum
 
+from loguru import logger
+import logging
+
+# logger.add(BASE_DIR/'run.log', serialize='json')
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        logger_opt = logger.opt(depth=6, exception=record.exc_info)
+        logger_opt.log(record.levelname, record.getMessage())
+
+
+logger_name_list = [name for name in logging.root.manager.loggerDict]
+
+for logger_name in logger_name_list:
+    logging.getLogger(logger_name).setLevel(10)
+    logging.getLogger(logger_name).handlers = []
+    logging.getLogger(logger_name).addHandler(InterceptHandler())
+
+
 FULL_FORMAT = ('<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> '
-               '<red>{extra[project_name]}</red> '
+               # '<red>{extra[project_name]}</red> '
                '[PID:{process}] '
                '[TID:{thread}] '
                '<level>[{level}]</level> '
                '[{file.path}:{function}:{line}] '
                '- '
-               '<yellow>[RID:{extra[trace_id]}]</yellow> '
-               '<magenta>[{extra[log_name]}]</magenta> '
+               # '<yellow>[RID:{extra[trace_id]}]</yellow> '
+               # '<magenta>[{extra[log_name]}]</magenta> '
                '<WHITE>{message}</WHITE>')
 
 SHORT_FORMAT = ('<green>[{time:HH:mm:ss.SSS}]</green> '
                 '<level>[{level}]</level> '
-                '<yellow>[TraceID:{extra[trace_id]}]</yellow> '
-                '<magenta>[{extra[log_name]}]</magenta> '
+                # '<yellow>[TraceID:{extra[trace_id]}]</yellow> '
+                # '<magenta>[{extra[log_name]}]</magenta> '
                 '{message}')
 
 logger.remove()
--- app/main.py
+++ app/main.py
@@ -1,4 +1,24 @@
-from .apiserver import myapp
+from loguru import logger
+import logging
+
+# logger.add(BASE_DIR/'run.log', serialize='json')
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        logger_opt = logger.opt(depth=6, exception=record.exc_info)
+        logger_opt.log(record.levelname, record.getMessage())
+
+
+logger_name_list = [name for name in logging.root.manager.loggerDict]
+
+for logger_name in logger_name_list:
+    logging.getLogger(logger_name).setLevel(10)
+    logging.getLogger(logger_name).handlers = []
+    logging.getLogger(logger_name).addHandler(InterceptHandler())
+
+
+from app.apiserver import myapp
 
 myapp = myapp
 
