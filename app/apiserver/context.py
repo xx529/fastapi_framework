3a06557f2375924e0ef438caf9ee4f6e5829f46e
Hang <64747715+xx529@users.noreply.github.com>
2024-04-10 20:50:26 +0800

    增加步骤记录

--- app/apiserver/context.py
+++ app/apiserver/context.py
@@ -2,6 +2,7 @@ import contextvars
 import uuid
 
 trace_id_var = contextvars.ContextVar('trace_id')
+step_num_var = contextvars.ContextVar('step_num')
 
 
 class RequestCtx:
@@ -18,3 +19,23 @@ class RequestCtx:
         trace_id = uuid.uuid4().hex
         trace_id_var.set(trace_id)
         return trace_id
+
+
+class LoggerStep:
+
+    @staticmethod
+    def get_step_num() -> int:
+        try:
+            LoggerStep.add()
+            return step_num_var.get()
+        except Exception:
+            return None
+
+    @staticmethod
+    def add():
+        num = step_num_var.get() + 1
+        step_num_var.set(num)
+
+    @staticmethod
+    def reset_step_num():
+        step_num_var.set(0)
