1eb42ed777c4bc42ee83503d06543515d4b87868
Hang <64747715+xx529@users.noreply.github.com>
2024-05-15 23:00:52 +0800

    优化config

--- app/main.py
+++ app/main.py
@@ -5,9 +5,11 @@ myapp = myapp
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app='main:myapp',
-                host='localhost',
-                port=8000,
-                reload=True,
-                app_dir='app',
-                workers=5)
+    uvicorn.run(
+        app='main:myapp',
+        host='localhost',
+        port=8000,
+        reload=True,
+        app_dir='app',
+        workers=5
+    )
