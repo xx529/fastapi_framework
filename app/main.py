c5b3de6bed19709f648e3a10a2dd80c267eec17b
Hang <64747715+xx529@users.noreply.github.com>
2024-05-19 15:46:34 +0800

    优化 ignore

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     uvicorn.run(
         app='main:myapp',
-        host='localhost',
+        host='0.0.0.0',
         port=8000,
         reload=True,
         app_dir='app',
