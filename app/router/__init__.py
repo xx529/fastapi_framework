7701344b1b83c82ed8811f9c08b3c05bc6b9d825
Hang <64747715+xx529@users.noreply.github.com>
2024-07-01 20:56:35 +0800

    merge adb and db transition

--- app/router/__init__.py
+++ app/router/__init__.py
@@ -1,9 +1,18 @@
+from fastapi import APIRouter
+
 from app.router import (
     stream_router,
     system_router,
     task_router,
     user_router,
 )
+
+routers = APIRouter()
+routers.include_router()
+routers.include_router(task_router.router)
+routers.include_router(user_router.router)
+routers.include_router(stream_router.router)
+routers.include_router(system_router.router)
 
 all_routers = [
     task_router.router,
