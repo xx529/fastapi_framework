2733b22116b7588b524249e6d93055f43ffc254c
Hang <64747715+xx529@users.noreply.github.com>
2024-07-05 20:45:28 +0800

    merge adb and db transition

--- app/apiserver/context.py
+++ app/apiserver/context.py
@@ -1,10 +1,6 @@
 import contextvars
-import uuid
 
 from pydantic import BaseModel, Field
-
-trace_id_var = contextvars.ContextVar('trace_id')
-ctx_var = contextvars.ContextVar('ctx')
 
 
 class ContextInfo(BaseModel):
@@ -13,17 +9,18 @@ class ContextInfo(BaseModel):
 
 
 class RunContext:
+    CTX_VAR = contextvars.ContextVar('ctx')
 
     def __init__(self, ctx: ContextInfo):
         self.ctx = ctx
         self.token = None
 
     def __enter__(self):
-        self.token = ctx_var.set(self.ctx.model_copy(deep=True))
-        return self.ctx
+        self.token = self.CTX_VAR.set(self.ctx.model_copy(deep=True))
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        ctx_var.reset(self.token)
+        self.CTX_VAR.reset(self.token)
         if exc_type:
             raise exc_val
 
@@ -35,42 +32,22 @@ class RunContext:
     @classmethod
     def current(cls) -> ContextInfo | None:
         try:
-            return ctx_var.get()
+            return cls.CTX_VAR.get()
         except Exception as _:
             return ContextInfo()
-
-
-class RequestCtx:
-
-    @staticmethod
-    def get_trace_id() -> str | None:
-        try:
-            return trace_id_var.get()
-        except Exception:
-            return None
-
-    @staticmethod
-    def create_trace_id() -> str:
-        trace_id = uuid.uuid4().hex
-        trace_id_var.set(trace_id)
-        return trace_id
-
-    @staticmethod
-    def set_trace_id(trace_id) -> None:
-        trace_id_var.set(trace_id)
 
 
 if __name__ == '__main__':
     t1 = ContextInfo(trace_id='a', user_id=1)
     print(RunContext.current())
 
-    with RunContext(ctx=t1):
+    with RunContext(ctx=t1) as r1:
         print(RunContext.current())
 
-        with RunContext.update(trace_id='bbb'):
+        with r1.update(trace_id='bbb') as r2:
             print(RunContext.current())
 
-            with RunContext.update(user_id=999):
+            with r2.update(user_id=999) as r3:
                 print(RunContext.current())
 
             print(RunContext.current())
