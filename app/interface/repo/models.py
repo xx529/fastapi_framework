a7ec6971d51e1b83190ac5706dc5a7ed5476b883
Hang <64747715+xx529@users.noreply.github.com>
2024-07-12 17:56:57 +0800

    add sql model

--- app/interface/repo/models.py
+++ app/interface/repo/models.py
@@ -1,2 +1,10 @@
-from sqlmodel import SQLModel
+from sqlmodel import Field, SQLModel
 
+
+class User(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    name: str = Field(max_length=100)
+
+
+print(User(id=1, name='a'))
+print(User(id=1, name='a').model_dump())
