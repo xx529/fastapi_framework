c1c64bd6f697464981c553c3b8fe91082ae72f56
Hang <64747715+xx529@users.noreply.github.com>
2024-07-13 23:10:39 +0800

    add sql model

--- app/interface/repo/models.py
+++ app/interface/repo/models.py
@@ -1,7 +1,19 @@
+from datetime import datetime
+
 from sqlmodel import Field, SQLModel
 
 
-class User(SQLModel, table=True):
+class BaseModel(SQLModel):
+    id: int = Field(primary_key=True)
+    create_at: datetime = Field(max_length=100)
+    create_by: int = Field(max_length=100)
+    update_at: datetime = Field(max_length=100)
+    update_by: int = Field(max_length=100)
+    deleted_at: datetime = Field(max_length=100)
+    deleted_by: int = Field(default=False)
+
+
+class User(BaseModel, table=True):
     id: int = Field(primary_key=True)
     name: str = Field(max_length=100)
 
