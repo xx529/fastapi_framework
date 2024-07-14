709f9514dcf7160bde0f54bcf0777e86b1784585
Hang <64747715+xx529@users.noreply.github.com>
2024-07-14 22:24:33 +0800

    add sql model

--- app/interface/repo/models.py
+++ app/interface/repo/models.py
@@ -13,9 +13,9 @@ class IntId(SQLModel):
 
 class BaseModel(SQLModel):
     id: int = Field(primary_key=True)
-    create_at: datetime = Field(max_length=100)
+    create_at: datetime = Field(default_factory=datetime.now, max_length=100)
     create_by: int = Field(max_length=100)
-    update_at: datetime = Field(max_length=100)
+    update_at: datetime = Field(default_factory=datetime.now, max_length=100)
     update_by: int = Field(max_length=100)
     deleted_at: datetime = Field(max_length=100)
     deleted_by: int = Field(default=False)
